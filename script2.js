bdde8fe0be5294157ebff1e99cd03b755c98b382
handada <31514625+handada@users.noreply.github.com>
2017-09-04 18:22:52 +0900

    9/4 at the school
    과제 수정 및 완료

--- script2.js
+++ script2.js
@@ -1,7 +1,8 @@
-function add(a,b,c){
-  return a + b + c;
- }
-   function(){
+function add(a,b,c) {
+    return a + b + c;
+}
+
+function total() {
     var int1 = document.getElementById('first').value;
     var int2 = document.getElementById('second').value;
     var int3 = document.getElementById('third').value;
@@ -9,15 +10,31 @@ function add(a,b,c){
     var v1 = Number(int1);
     var v2 = Number(int2);
     var v3 = Number(int3);
-    
-    answer.value = add(v1,v2,v3);
-       
+    if(isNaN(v1 + v2 + v3)){
+        alert("숫자만 적어 시키야");
+    }else{
+        answer.value=add(v1,v2,v3);
+    }
 
     if (answer.value <= 7){
-      document.getElementById('picture').src="images/andae.jpg";
+        document.getElementById('picture').src="images/andae.jpg";
     }else if(answer.value > 7 && answer.value <= 23){
-      document.getElementById('picture').src="images/taltal.png";
+        document.getElementById('picture').src="images/taltal.png";
     }else {
         document.getElementById('picture').src="images/gae.jpg";
     }
-  });
+}
+
+(function() {
+    var inputTag = document.getElementsByTagName('input');
+    inputTag[0].addEventListener(
+        'keyup', total);
+    inputTag[1].addEventListener('keyup', total);
+    inputTag[2].addEventListener('keyup', total);
+})();
+
+var al = document.createElement('a');
+al.id = "dkd";
+al.href = "dkjdflddl.dlkdjld";
+al.innerText="dkkdk"
+document.querySelector('body').appendChild(al);
